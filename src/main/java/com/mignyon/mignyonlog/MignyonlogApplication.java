76be479b7d1744adda1e16d01b5e1429ded45006
Jeongyeon Kim <wjddus9132@gmail.com>
2025-02-08 09:46:10 +0900

    init: first commit

---
+++ src/main/java/com/mignyon/mignyonlog/MignyonlogApplication.java
@@ -0,0 +1,13 @@
+package com.mignyon.mignyonlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MignyonlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MignyonlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mignyon/mignyonlog/MignyonlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mignyon.mignyonlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MignyonlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
